a0acb21ebdc3a573669a19f6e53180023b7e4394
sturvi <zlbaqnnvijd@internetkeno.com>
2023-07-20 22:51:54 +0400

    Tests have been written for the

---
+++ src/test/java/com/example/englingbot/service/handlers/implementations/MessageHandlerTests.java
@@ -0,0 +1,110 @@
+package com.example.englingbot.service.handlers.implementations;
+
+import com.example.englingbot.model.UserWordList;
+import com.example.englingbot.model.enums.UserStateEnum;
+import com.example.englingbot.model.enums.WordListTypeEnum;
+import com.example.englingbot.service.UserService;
+import com.example.englingbot.service.UserWordListService;
+import com.example.englingbot.service.enums.TextCommandsEnum;
+import com.example.englingbot.service.externalapi.telegram.BotEvent;
+import com.example.englingbot.service.sendmessage.SendMessageForUser;
+import com.example.englingbot.service.sendmessage.SendMessageForUserFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import static org.mockito.Mockito.*;
+
+class MessageHandlerTest {
+
+    @Spy
+    @InjectMocks
+    private MessageHandler messageHandler;
+
+    @Mock
+    private SendMessageForUserFactory sendMessageForUserFactory;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private UserWordListService userWordListService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        messageHandler.init();
+    }
+
+    @Test
+    void handleMessage_startCommand() {
+        BotEvent botEvent = mock(BotEvent.class);
+        when(botEvent.getId()).thenReturn(123L);
+        when(botEvent.getText()).thenReturn(TextCommandsEnum.START.getCommand());
+        var sendMessageForUser = Mockito.mock(SendMessageForUser.class);
+        when(sendMessageForUserFactory.createMessageSender()).thenReturn(sendMessageForUser);
+        doNothing().when(sendMessageForUser).sendMessage(anyLong(), anyString());
+
+        messageHandler.handle(botEvent);
+
+        verify(userService, times(1)).changeAppUserState(UserStateEnum.MAIN, botEvent);
+        verify(sendMessageForUserFactory, times(1)).createMessageSender();
+        verify(sendMessageForUser, times(1)).sendMessage(eq(123L), anyString());
+    }
+
+    @Test
+    void handleMessage_answerCommand() {
+        BotEvent botEvent = mock(BotEvent.class);
+        when(botEvent.getId()).thenReturn(123L);
+        when(botEvent.getText()).thenReturn(TextCommandsEnum.ANSWER.getCommand());
+
+        messageHandler.handle(botEvent);
+
+        verify(userService, times(1)).changeAppUserState(UserStateEnum.ANSWER, botEvent);
+        verify(sendMessageForUserFactory, times(1)).createMessageSender();
+        verify(sendMessageForUserFactory, times(1)).createMessageSender().sendMessage(eq(123L), anyString());
+    }
+
+    @Test
+    void handleMessage_addWordCommand() {
+        BotEvent botEvent = mock(BotEvent.class);
+        when(botEvent.getId()).thenReturn(123L);
+        when(botEvent.getText()).thenReturn(TextCommandsEnum.ADD_WORD.getCommand());
+
+        messageHandler.handle(botEvent);
+
+        verify(userService, times(1)).changeAppUserState(UserStateEnum.ADD_MENU, botEvent);
+        verify(sendMessageForUserFactory, times(1)).createMessageSender();
+        verify(sendMessageForUserFactory, times(1)).createMessageSender().sendMessage(eq(123L), anyString());
+    }
+
+    @Test
+    void handleMessage_learnWordCommand_noWordsToLearn() {
+        BotEvent botEvent = mock(BotEvent.class);
+        when(botEvent.getId()).thenReturn(123L);
+        when(botEvent.getText()).thenReturn(TextCommandsEnum.LEARN_WORD.getCommand());
+        when(userWordListService.getRandomUserWordList(any(), eq(WordListTypeEnum.LEARNING))).thenReturn(null);
+
+        messageHandler.handle(botEvent);
+
+        verify(userService, times(1)).getAppUser(botEvent);
+        verify(userWordListService, times(1)).getRandomUserWordList(any(), eq(WordListTypeEnum.LEARNING));
+        verify(sendMessageForUserFactory, times(1)).createMessageSender();
+        verify(sendMessageForUserFactory, times(1)).createMessageSender().sendMessage(eq(123L), anyString());
+    }
+
+    @Test
+    void handleMessage_learnWordCommand_hasWordsToLearn() {
+        BotEvent botEvent = mock(BotEvent.class);
+        when(botEvent.getId()).thenReturn(123L);
+        when(botEvent.getText()).thenReturn(TextCommandsEnum.LEARN_WORD.getCommand());
+        when(userWordListService.getRandomUserWordList(any(), eq(WordListTypeEnum.LEARNING))).thenReturn(any(UserWordList.class));
+
+        messageHandler.handle(botEvent);
+
+        verify(userService, times(1)).getAppUser(botEvent);
+        verify(userWordListService, times(1)).getRandomUserWordList(any(), eq(WordListTypeEnum.LEARNING));
+        verify(sendMessageForUserFactory, times(1)).createMessageSender();
+        verify(sendMessageForUserFactory, times(1)).createMessageSender().sendMessage(eq(123L), anyString());
+    }
+}
---
+++ src/test/java/com/example/englingbot/service/sendmessage/SendMessageForUserTest.java
@@ -0,0 +1,41 @@
+package com.example.englingbot.service.sendmessage;
+
+import com.example.englingbot.service.externalapi.telegram.TelegramBotApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
+import static org.mockito.Mockito.*;
+
+public class SendMessageForUserTest {
+
+    @Mock
+    private TelegramBotApplication telegramBotApplication;
+
+    @InjectMocks
+    private SendMessageForUser sendMessageForUser;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        sendMessageForUser = new SendMessageForUser(telegramBotApplication);
+    }
+
+    @Test
+    void testSendMessage() throws Exception {
+        Long chatId = 123L;
+        String messageText = "test message";
+        Message returnedMessage = mock(Message.class);
+
+        when(telegramBotApplication.execute(any(SendMessage.class))).thenReturn(returnedMessage);
+
+        sendMessageForUser.sendMessage(chatId, messageText);
+
+        verify(telegramBotApplication, times(1)).execute(any(SendMessage.class));
+    }
+}
