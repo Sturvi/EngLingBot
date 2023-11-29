af0e6d60b9c4dd21774f7cbdc90fa60539ac37bd
emin.b <emin@baghirov.su>
2023-11-29 17:03:19 +0400

    .

--- src/main/java/com/example/englingbot/EngLingBotApplication.java
+++ src/main/java/com/example/englingbot/EngLingBotApplication.java
@@ -1,5 +1,8 @@
 package com.example.englingbot;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
-public class EngLingBotApplication {
+@Theme(value = "my-app", variant = Lumo.DARK)
+public class EngLingBotApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         log.info("The application is starting.");
         SpringApplication.run(EngLingBotApplication.class, args);
     }
+
 }
