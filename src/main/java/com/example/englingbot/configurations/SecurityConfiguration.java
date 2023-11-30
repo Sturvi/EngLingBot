28c29a5094756da33a24aa69502863601aac519c
Sturvi <sturvi4@gmail.com>
2023-11-30 19:00:13 +0400

    Added security configurations and update views
    This commit includes updates to existing views, the implementation of a simple security setup, and configuration changes. The security setup introduces a new SecurityConfiguration with a basic in-memory user manager and enables `/logout` route. For updating views, permissions were defined using `@AnonymousAllowed` and `@PermitAll` annotations for specific endpoints like "About" and "Hello". A ‘login' view was also introduced. Changes in configurations include specifying Vaadin's `whitelisted-packages` and `ignoredPaths` in `application.properties`, and adding bot credentials in dev.properties. The addition of `spring-boot-starter-security` dependency in `pom.xml` enables Spring Security features.

---
+++ src/main/java/com/example/englingbot/configurations/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.englingbot.configurations;
+
+import com.example.englingbot.frontend.views.login.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends VaadinWebSecurity {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
+
+        http.logout()
+                .logoutSuccessUrl("/login") // URL для перенаправления после выхода
+                .invalidateHttpSession(true) // Инвалидация сессии при выходе
+                .deleteCookies("JSESSIONID"); // Удаление куки сессии
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager (PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password(passwordEncoder.encode("admin1234"))
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
